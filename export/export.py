9e83d69833f14f3d0d5d1a70de4b64555e9a8b7b
Charan Venkat Chandran <chandran7824@students.d211.org>
2025-01-08 23:23:48 -0600

    ended up saving both the timeline and component tree

--- export/export.py
+++ export/export.py
@@ -45,6 +45,8 @@ def run(context):
         output_folder = os.path.basename(os.path.normpath(src_folder_path))
 
         data = read_timeline_data()
+        data["json"] = compress_json(data)
+
         os.makedirs(os.path.dirname(src_folder_path), exist_ok=True)
         with open(os.path.join(src_folder_path, "timeline.json"), "w", encoding="utf-8") as f:
             json.dump(data, f, indent=2)
@@ -55,22 +57,29 @@ def run(context):
 
 
 def get_component_name(occurrence: adsk.fusion.Occurrence) -> str:
-    return f"{occurrence.timelineObject.index}{occurrence.component.name}-{occurrence.component.id}"
+    if occurrence.isReferencedComponent:
+        return f"{occurrence.component.name}-{occurrence.component.id[:5]}"
+    else:
+        return f"{occurrence.timelineObject.index}{occurrence.component.name}-{occurrence.component.id[:5]}"
 
 
 def read_timeline_data(design=design):
     # Instead of storing data in fusion attributes using a dict here is going to be faster
     data = {
-        design.rootComponent.id: {
-            "path": "",
-            "name": design.rootComponent.name,
-            "units": create_readable_value(
-                design.fusionUnitsManager.defaultLengthUnits,
-                cast(Literal[0, 1, 2, 3, 4], design.fusionUnitsManager.distanceDisplayUnits),
-            ),
-            "features": [],
-            "references": [],
-        }
+        "timeline": [],
+        "components": {
+            design.rootComponent.id: {
+                "path": "",
+                "is_linked": False,
+                "name": design.rootComponent.name,
+                "units": create_readable_value(
+                    design.fusionUnitsManager.defaultLengthUnits,
+                    cast(Literal[0, 1, 2, 3, 4], design.fusionUnitsManager.distanceDisplayUnits),
+                ),
+                "feature_index": [],
+                "references": [],
+            }
+        },
     }
 
     for timeline_item in design.timeline:
@@ -80,158 +89,38 @@ def read_timeline_data(design=design):
             feature = cast(
                 adsk.fusion.Feature, entity
             )  # If I try to cast using fusion api it deletes parentComponent for some reason
-            parent_component = feature.parentComponent
-            # print_fusion(parent_component.name)
-            data[parent_component.id]["features"].append({"name": feature.name, "type": feature.objectType})
+            data["timeline"].append(get_timeline_feature(timeline_item))
+            data["components"][feature.parentComponent.id]["feature_index"].append(timeline_item.index)
         elif hasattr(
             entity, "sourceComponent"
         ):  # This is an occurrence - Reasons for an occurence - 1. creation of a component - 2. reference of a component (copy and paste) - 3. reference of a component (linked)
             occurrence = adsk.fusion.Occurrence.cast(entity)
 
-            print_fusion(occurrence.sourceComponent.name)
+            data["timeline"].append(get_timeline_feature(timeline_item))
+
             # This is for the creation of a component
             if occurrence.component.id not in data:
-                parent_path = data[occurrence.sourceComponent.id]["path"]
+                parent_path = data["components"][occurrence.sourceComponent.id]["path"]
 
-                data[occurrence.component.id] = {
+                data["components"][occurrence.component.id] = {
                     "path": os.path.join(parent_path, get_component_name(occurrence)),
+                    "is_linked": occurrence.isReferencedComponent,
                     "name": design.rootComponent.name,
                     "units": create_readable_value(
                         design.fusionUnitsManager.defaultLengthUnits,
                         cast(Literal[0, 1, 2, 3, 4], design.fusionUnitsManager.distanceDisplayUnits),
                     ),
-                    "features": [],
+                    "feature_index": [],
                     "references": [],
                 }
             else:
-                parent_path = data[occurrence.sourceComponent.id]["path"]
-                data[occurrence.component.id]["references"].append(
+                parent_path = data["components"][occurrence.sourceComponent.id]["path"]
+                data["components"][occurrence.component.id]["references"].append(
                     {"name": occurrence.name, "path": os.path.join(parent_path, get_component_name(occurrence))}
                 )
 
-            data[occurrence.sourceComponent.id]["features"].append(
-                {"name": occurrence.name, "type": occurrence.objectType}
-            )
-
-    # Only the root component timeline has the json data
-    # Though might wanna change it back to every file since makes sense for diffs
-    # data["json"] = compress_json(data)
+            data["components"][occurrence.sourceComponent.id]["feature_index"].append(timeline_item.index)
     return data
-
-
-def get_component_timeline_data(doc=design) -> FusionComponentTimeline:
-    component_timeline: FusionComponentTimeline = {doc.rootComponent.id: []}
-    doc.attributes.add("CHURRO-EXPORT", doc.rootComponent.id, "")
-
-    for timeline_item in doc.timeline:
-        entity = timeline_item.entity
-
-        if hasattr(entity, "parentComponent"):  # This is a feature
-            feature = cast(
-                adsk.fusion.Feature, entity
-            )  # If I try to cast using fusion api it deletes parentComponent for some reason
-            parent_component = feature.parentComponent
-            component_timeline[parent_component.id].append(timeline_item)
-        elif hasattr(
-            entity, "sourceComponent"
-        ):  # This is an occurrence - Reasons for an occurence - 1. creation of a component - 2. reference of a component (copy and paste) - 3. reference of a component (linked)
-            occurrence = adsk.fusion.Occurrence.cast(entity)
-
-            if (
-                occurrence.component.id not in component_timeline and not occurrence.isReferencedComponent
-            ):  # If this is the first occurence of the component we know that this is a creation - Cause copy paste needs the inital component to be created
-                parent_path = doc.attributes.itemByName("CHURRO-EXPORT", occurrence.sourceComponent.id).value
-                doc.attributes.add(
-                    "CHURRO-EXPORT",
-                    occurrence.component.id,
-                    os.path.join(
-                        parent_path,
-                        f"{occurrence.timelineObject.index}{occurrence.component.name}-{occurrence.component.id}",
-                    ),
-                )
-
-            component_timeline.setdefault(occurrence.component.id, [])
-            component_timeline[occurrence.sourceComponent.id].append(
-                timeline_item
-            )  # Here we add it to the timeline of the parent component
-    return component_timeline
-
-
-def traverseAssembly(occurrences: adsk.fusion.OccurrenceList, path, component_timeline: FusionComponentTimeline):
-    for occurrence in occurrences:
-        try:
-            # I check if component is not linked before adding occurrence.timelineObject.index since there seems to be an error in fusio
-            # Also since a linked component will all reference the same component we're bing chilling - Since we need the index since the same level component
-            # Could cause the folder to be overwritten as a reference - Which will never happen to a linked component since they all have the same folder
-            folder_name = f"{occurrence.timelineObject.index if not occurrence.isReferencedComponent else ""}{occurrence.component.name}-{occurrence.component.id}"
-            # ACTUALLY SCRATCH THAT - I'll Still need the index for linked components since they can be at the same level
-            # Just don't need it for the respresentation in the linked_components folder
-            occurrence_path = os.path.join(path, folder_name)
-            original_component_occurence = root.allOccurrencesByComponent(occurrence.component).item(0)
-            if occurrence == original_component_occurence:
-                # This is the creation of the component
-                write_component_data_to_file(occurrence, path, component_timeline)
-            else:
-                if (
-                    not occurrence.isReferencedComponent
-                ):  # Since if this is the second reference to a link it don't need to run cause it's in linked_components already
-                    write_component_data_to_file(occurrence, path, component_timeline, True)
-
-            # If it's a linked component we don't want to read further in for now
-            if occurrence.childOccurrences and not occurrence.isReferencedComponent:
-                traverseAssembly(occurrence.childOccurrences, occurrence_path, component_timeline)
-        except Exception as e:
-            error("Failed to traverse assembly", e)
-
-
-def write_component_data_to_file(
-    occurrence: adsk.fusion.Occurrence,
-    folder_path: str,
-    component_timeline: FusionComponentTimeline,
-    component_reference=False,
-):
-    data: Timeline = {
-        "document_name": occurrence.component.name,
-        "units": create_readable_value(
-            units_manager.defaultLengthUnits, cast(Literal[0, 1, 2, 3, 4], units_manager.distanceDisplayUnits)
-        ),
-        "features": [],
-    }
-
-    final_folder_path = os.path.join(src_folder_path, folder_path)
-
-    # Regular component with features
-    if not component_reference and not occurrence.isReferencedComponent:
-        data["features"] = [
-            get_timeline_feature(timeline_feature) for timeline_feature in component_timeline[occurrence.component.id]
-        ]
-
-    # Component reference
-    # f"[{component_details['name']}]({component_details['path'].replace(' ', '%20').replace('\\', '/')}/timeline.md)"
-    elif component_reference:
-        data["info"] = {
-            "link_to_component": f"[{occurrence.component.name}](/{os.path.join(output_folder, design.attributes.itemByName("CHURRO-EXPORT", occurrence.component.id).value, "timeline.md").replace(' ', '%20').replace('\\', '/')})",
-            "component_reference": True,
-            "component_reference_id": occurrence.component.id,
-        }
-
-    # Linked component
-    elif occurrence.isReferencedComponent:
-        data["info"] = {
-            "link_to_component": f"[{occurrence.component.name}](/{os.path.join(output_folder, "linked_components", folder_path, "timeline.md").replace(' ', '%20').replace('\\', '/')})",
-            "component_reference": False,
-            "component_reference_id": occurrence.component.id,
-        }
-        # traverseAssembly(
-        #     occurrence.childOccurrences,
-        #     os.path.join(src_folder_path, "linked_components", os.path.basename(folder_path)),
-        #     get_component_timeline_data(occurrence.component.parentDesign),
-        # )
-        # write_to_file(
-        #     os.path.join(src_folder_path, "linked_components", os.path.basename(folder_path), "timeline.md"), data
-        # )
-
-    # write_to_file(os.path.join(final_folder_path, "timeline.md"), data)
 
 
 def get_timeline_feature(timeline_feature: adsk.fusion.TimelineObject) -> Feature | Error:
